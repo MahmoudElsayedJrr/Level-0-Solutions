0aeebeddcfc699c9c5b437fa35c99e999b2c2b92
daddy <56431117+Mahmoudelsayed00@users.noreply.github.com>
2021-07-25 13:44:08 +0200

    Update A-Crazy Sum.cpp

--- Contest #2/A-Crazy Sum.cpp
+++ Contest #2/A-Crazy Sum.cpp
@@ -4,13 +4,16 @@ using namespace std ;
 
 int main(){     
 
-       long long n ,  k ,  sum = 0 ; cin >> n >> k;
+       long long n ,  k ,  sum = 0 ;
+        cin >> n >> k;
 
           for( int i = 0 ; i < n ; i++){
-          long long z ; cin >> z;
+          long long z ; 
+           cin >> z;
            sum = ((sum % k) + (z % k)) % k;
           }
-          cout << ( sum % k == 0 ? "YES" : "NO");
+          if (sum % k == 0) cout << "YES";
+          else cout << "NO";
 
 
 
